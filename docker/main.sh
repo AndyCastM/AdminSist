a7d79c2dc738e4a914d4471d9ec6cf77b0982c72
AndreaCast <acastellnosmar@gmail.com>
2025-05-13 22:22:44 -0700

    otro cambio

--- docker/main.sh
+++ docker/main.sh
@@ -8,44 +8,48 @@ fi
 source "./dockerconfig.sh"
 
 echo "--- Bienvenido a la configuración de Docker ---"
-echo "Instalando Docker..."
-instalar_docker
 
 while true; do
     echo "Seleccione la opción que desea configurar:"
-    echo "1) Montar Apache"
-    echo "2) Personalizar imagen de Apache"
-    echo "3) Clonar la imagen de Apache"
-    echo "4) Crear conexión entre contenedores"
-    echo "5) Verificar contenedores"
-    echo "6) Salir"
+    echo "1) Instalar Docker"
+    echo "2) Montar Apache"
+    echo "3) Personalizar imagen de Apache"
+    echo "4) Clonar la imagen de Apache"
+    echo "5) Crear conexión entre contenedores"
+    echo "6) Verificar contenedores"
+    echo "7) Salir"
     read -p "Elija una opción (1-5): " op2
     case "$op2" in
         1)
-            echo "Opción 1 seleccionada: Montar Apache"
+            echo "Opción 1 seleccionada: Instalar Docker"
+            echo "Instalando Docker..."
+            instalar_docker
+            ;;
+        2)
+            echo "Opción 2 seleccionada: Montar Apache"
             echo "Montando Apache..."
             montar_apache
             ;;
-        2)
-            echo "Opción 2 seleccionada: Personalizar imagen de Apache"
+        3)
+            echo "Opción 3 seleccionada: Personalizar imagen de Apache"
             echo "Modificando Apache..."
             modificar_apache
             ;;
-        3)
-            echo "Opción 3 seleccionada: Clonar la imagen de Apache"
+        4)
+            echo "Opción 4 seleccionada: Clonar la imagen de Apache"
             crear_dockerfile
             ;;
-        4)
-            echo "Opción 4 seleccionada: Crear conexión entre contenedores"
+        5)
+            echo "Opción 5 seleccionada: Crear conexión entre contenedores"
             echo "Creando conexión entre contenedores..."
             contenedores_postgres
             ;;
-        5)
-            echo "Opción 5 seleccionada: Verificar contenedores"
+        6)
+            echo "Opción 6 seleccionada: Verificar contenedores"
             comunicacion_contenedores
             ;;
-        6)
-            echo "Opción 5 seleccionada: Salir"
+        7)
+            echo "Opción 7 seleccionada: Salir"
             echo "Saliendo..."
             exit 0
             ;;
